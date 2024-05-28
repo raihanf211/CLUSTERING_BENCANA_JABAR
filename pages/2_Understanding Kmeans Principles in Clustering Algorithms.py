c2e958e5e9c325a4e40e3342876d7f1845e00a1e
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-05-28 22:12:25 +0700

    Update 2_Understanding Kmeans Principles in Clustering Algorithms.py

--- pages/2_Understanding Kmeans Principles in Clustering Algorithms.py
+++ pages/2_Understanding Kmeans Principles in Clustering Algorithms.py
@@ -10,7 +10,7 @@ import plotly_express as px
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Load the dataset
-df = pd.read_csv("UPDATE-Selection-Dataset_Longsor 2021 - 2023 - PROV JABAR.csv")
+df = pd.read_csv("DATA_JABAR.csv")
 
 #navicon and header
 st.set_page_config(page_title="Dashboard", page_icon="📈", layout="wide")  
