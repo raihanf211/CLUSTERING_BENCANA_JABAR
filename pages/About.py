fb6753781c8b74c50e826870107cc60ce41993f1
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-06-14 14:47:21 +0700

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -48,10 +48,8 @@ html_content = f"""
     
     <p> Berdasarkan data Badan Nasional Penanggulangan Bencana (BNPB) melaporkan ada 3.531 bencana alam. Berikut daftar lengkap 10 provinsi yang paling banyak mengalami bencana alam di Indonesia pada 2022 diantaranya, Jawa Barat 823 kejadian.</p>
     <p> Berdasarkan data Badan Nasional Penanggulangan Bencana (BNPB), Jawa Barat  menjadi provinsi dengan bencana tanah longsor terbanyak dalam sedekade terakhir. Kejadian tanah longsor selama periode 2014-2023. Diantaranya Jawa Barat 2.440 Kejadian.</p>
-    <p> Dalam menghadapi bencana alam, kesiapsiagaan menjadi kunci utama dalam meminimalisir dampak yang ditimbulkan. Masyarakat khususnya di Jawa Barat sebagai elemen terdepan harus memiliki kemampuan untuk bersiap dan bertindak saat bencana terjadi. </p> 
-    <p> Kecepatan dan ketepatan dalam penanggulangan bencana sangatlah krusial untuk menjamin keberhasilan prosesnya. Untuk mengurangi semua kerentanan fisik dan sosial ekonomi, kesiapsiagaan bencana menjadi pendekatan penting.</p>
-    <p> Melalui analisis komprehensif terhadap bencana longsor dan pemahaman yang mendalam terhadap dampaknya, serta dengan penggunaan pengelompokkan dan pemetaan yang akurat, dapat dicapai perencanaan mitigasi yang optimal dan pengembangan kebijakan yang lebih efektif di tingkat provinsi atau nasional.</p>
-    <p> Untuk mengidentifikasi daerah rawan bencana tanah longsor di Provinsi Jawa Barat, diperlukan pemanfaatan data dengan melakukan penerapan algoritma pengelompokkan data. Dua algoritma yang digunakan adalah K-means dan Agglomerative Hierarchical Clustering (AHC), yang memungkinkan pengelompokkan data bencana tanah longsor menjadi kelompok yang berbeda berdasarkan karakteristiknya.</p>
+    <p> Dalam menghadapi bencana alam, kesiapsiagaan menjadi kunci untuk meminimalisir dampak. Masyarakat di Jawa Barat harus siap dan mampu bertindak saat bencana terjadi. Kecepatan dan ketepatan penanggulangan sangat penting untuk mengurangi kerentanan fisik dan sosial ekonomi. </p> 
+    <p> Melalui analisis komprehensif dan pemetaan akurat, perencanaan mitigasi dan pengembangan kebijakan yang efektif dapat dicapai. Untuk mengidentifikasi daerah rawan bencana tanah longsor di Jawa Barat, algoritma K-means dan Agglomerative Hierarchical Clustering (AHC) digunakan untuk mengelompokkan data berdasarkan karakteristiknya.</p>
 """
 
 # Tampilkan konten HTML
